9f19db5f2a84f09986d5de05bcd84d5c9e326460
n0p <lbernal@gmail.com>
2014-04-03 16:48:06 +0200

    Script para emitir usando una webcam

---
+++ yt-webcamlive.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script para emitir en directo una webcam usando youtube
+# Script original de Juanjo2
+
+url1="rtmp://a.rtmp.youtube.com/live2/ENDPOINT"
+
+#watermark="watermark-new.png"
+ffmpeg="/usr/bin/ffmpeg"
+
+$ffmpeg \
+-f alsa -i hw:0,0 \
+-f video4linux2 -s 1280x720 -i /dev/video0 \
+-vcodec libx264 -preset ultrafast -threads 2 -b:v 5000k \
+-acodec aac -async 1 -strict experimental -g 2 \
+-r 30 -f flv "$url1" 
+
+#        -vf "movie=$watermark [watermark]; \
+#        [in] [watermark] overlay=5:5 [out]" \
+
+#-vf scale=-1:360 \
+
+#-vcodec libx264 -force_key_frames "expr:gte(t,n_forced*2)"  -preset ultrafast \
+#-acodec libmp3lame -async 1 \
