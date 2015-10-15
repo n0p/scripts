734cfbd6f26cdafcbda303a30e45fda4bfbd6bfc
n0p <lbernal@gmail.com>
2015-10-15 20:00:26 +0200

    Añadida key como argumento

--- yt-webcamlive.sh
+++ yt-webcamlive.sh
@@ -2,16 +2,16 @@
 # Script para emitir en directo una webcam usando youtube
 # Script original de Juanjo2
 
-url1="rtmp://a.rtmp.youtube.com/live2/ENDPOINT"
+url1="rtmp://a.rtmp.youtube.com/live2/$1"
 
 #watermark="watermark-new.png"
 ffmpeg="/usr/bin/ffmpeg"
 
 $ffmpeg \
 -f alsa -i hw:0,0 \
--f video4linux2 -s 1280x720 -i /dev/video0 \
+-f video4linux2 -s 1280x720 -i /dev/video1 \
 -vcodec libx264 -preset ultrafast -threads 2 -b:v 5000k \
--acodec aac -async 1 -strict experimental -g 2 \
+-acodec aac -async 1 -strict experimental -g 2 -ar 44100 -bufsize 512k -b:a 128k \
 -r 30 -f flv "$url1" 
 
 #        -vf "movie=$watermark [watermark]; \
